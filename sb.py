3e053283fc139efba6afc6cd9a1f28d2f1a31c01
surya27895 <surya_27895@yahoo.co.id>
2017-12-10 14:57:33 +0700

    Update sb.py

--- sb.py
+++ sb.py
@@ -49,7 +49,7 @@ while True:
                     if msg.toType == 2:
                         may = client.getProfile().mid
                         if may in str(msg.contentMetadata):
-                            pilih = ['yang tag sy semoga jomblo seumur hidup','ngapain tag tag woe, kangen?','ada apa ini? ko di tag?','duhh kena tag, dianya kesepian kali yah','gk usah tag, gift tikel aja']
+                            pilih = ['/join']
                             rslt = random.choice(pilih)
                             client.sendText(msg.to, str(rslt))
                         else:
