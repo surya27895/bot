4d2eef128f402df54ef0b0190fcd48835fab0142
surya27895 <surya_27895@yahoo.co.id>
2017-12-10 15:07:51 +0700

    Update sb.py

--- sb.py
+++ sb.py
@@ -52,6 +52,8 @@ while True:
                             pilih = ['/join']
                             rslt = random.choice(pilih)
                             client.sendText(msg.to, str(rslt))
+                        if msg.text = "/battle":
+                            client.sendText(msg.to, "/join")
                         else:
                             pass
                     else:
